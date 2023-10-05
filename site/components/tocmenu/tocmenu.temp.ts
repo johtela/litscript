f3bf90c6d93ae19499a76732923b54f34e839f74
johtela <tjohtela@gmail.com>
2023-10-06 00:12:40 +0300

    wip

--- site/components/tocmenu/tocmenu.temp.ts
+++ site/components/tocmenu/tocmenu.temp.ts
@@ -1,4 +1,4 @@
-import { html } from 'templates/html'
+import { html, css } from 'templates/html'
 import { Toc, TocEntry, relLink } from 'templates/toc'
 import { TemplateContext } from 'templates/template'
 
@@ -9,7 +9,7 @@ const tocTitle = (entry: TocEntry) =>
 
 const tocEntry = (entry: TocEntry, relFileName: string) => html`
     <li>
-        ${entry.subs ? '<button class="accordion">' : ''}
+        ${entry.subs ? '<div class="accordion">' : ''}
         ${entry.file ? 
             `<a href="${relLink(relFileName, entry.file)}"${
                 relFileName == entry.file ? 
@@ -27,8 +27,72 @@ const tocSection = (pages: Toc, relFileName: string) => html`
         ${pages.map(page => tocEntry(page, relFileName))}
     </ul>`
 
-export default (ctx: TemplateContext) => html`
+const styles = css`
+.tocmenu {
+    --highlight-bar-width: 3px;
+    --highlight-margin: 10px;
+    
+    margin: 10px;
+    color: var(--toc-text-color);
+    font-size: var(--toc-font-size);
+}
+.tocmenu h3 {
+    font-size: var(--toc-header-font-size);
+    margin: var(--toc-header-font-size) 0 10px 0;
+    --divider-color: var(--toc-header-ruler-color);
+}
+.tocmenu ul {
+    list-style-type: none;
+    overflow: hidden;
+    margin-left: calc(var(--toc-font-size) / 2);
+}
+.tocmenu ul ul {
+    margin-left: var(--toc-font-size);
+}
+.tocmenu li {
+    margin: calc(var(--toc-font-size) * 0.2) 0;
+}
+.tocmenu a {
+    text-decoration: none;
+    color: var(--toc-text-color);
+    display: inline-block;
+    width: 90%;
+    padding-left: var(--highlight-margin);
+}
+.tocmenu a.highlight {
+    border-left: var(--highlight-bar-width) solid var(--col-prim-dark);
+    padding-left: var(--highlight-margin) - var(--highlight-bar-width);
+    font-weight: bold;
+}
+.tocmenu a:active {
+    color: var(--toc-link-hover-color);
+}
+.tocmenu .accordion {
+    background-color: transparent;
+    color: var(--toc-text-color);
+    border-bottom: 2px solid var(--toc-sub-ruler-color);    
+    cursor: pointer;
+    padding: 2px 0;
+    margin-left: var(--highlight-margin);
+    width: 100%;
+}
+.tocmenu .accordion:after {
+    content: '\25B4';
+    color: var(--toc-sub-ruler-color);
+    font-weight: bold;
+    float: right;
+    margin: 0 var(--highlight-margin) 0 5px;
+}
+.tocmenu .accordion.collapsed:after {
+    content: "\25BE";
+}`
+
+export default (ctx: TemplateContext) => {
+    ctx.require('./tocmenu')
+    ctx.style(styles)
+    return html`
     <div class="tocmenu">
         <h3>${ctx.frontMatter.tocMenuHeader}</h3>
         ${tocSection(ctx.toc, ctx.relFilePath)}
     </div>`
+}
--- site/components/tocmenu/tocmenu.ts
+++ site/components/tocmenu/tocmenu.ts
@@ -1,6 +1,4 @@
-import "./tocmenu.less"
-import * as $ from "../../components/common/myquery"
-import { initAccordions } from "../../components/common/accordion"
+import * as $ from "components/common"
 
 let tocmenu = $.elementsWithStyle("tocmenu")[0] as HTMLElement
 if (tocmenu) {
@@ -8,4 +6,20 @@ if (tocmenu) {
         initAccordions(tocmenu)
     else
         document.fonts.onloadingdone = () => initAccordions(tocmenu)
+}
+
+export function initAccordions (element: HTMLElement) {
+    let accordions = element.getElementsByClassName($.accordion);
+
+    for (let i = 0; i < accordions.length; ++i) {
+        let acc = accordions[i] as HTMLElement;
+        let panel = acc.nextElementSibling as HTMLElement;
+        let initHeight = panel.scrollHeight + "px";
+        panel.style.maxHeight =  initHeight;
+        acc.onclick = () => {
+            acc.classList.toggle($.collapsed);
+            panel.style.maxHeight = panel.style.maxHeight === "0px" ? 
+                initHeight :  "0px";
+        }
+    }
 }
