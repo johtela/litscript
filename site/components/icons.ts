c46c65dd95710eb85c93efe94d9b36ef5710bf4d
Tommi Johtela <tommi.johtela@magicad.com>
2023-10-06 15:25:52 +0300

    wip

--- site/components/icons.ts
+++ site/components/icons.ts
@@ -29,5 +29,13 @@ export default {
             <path d="M15 21h-9a3 3 0 0 1 -3 -3v-1h10v2a2 2 0 0 0 4 0v-14a2 2 0 1 1 2 2h-2m2 -4h-11a3 3 0 0 0 -3 3v11" />
             <path d="M9 7l4 0" />
             <path d="M9 11l4 0" />
+        </svg>`,
+    caret_down: html`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-caret-down" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M6 10l6 6l6 -6h-12" />
+        </svg>`,
+    caret_up: html`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-caret-up" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M18 14l-6 -6l-6 6h12" />
         </svg>`
 }
--- site/components/tocmenu/tocmenu.temp.ts
+++ site/components/tocmenu/tocmenu.temp.ts
@@ -1,32 +1,45 @@
+/**
+ * # TOC Menu
+ * 
+ * Component that takes the Toc tree structure and outputs a navigatable tree.
+ */
 import { html, css } from 'templates/html'
 import { Toc, TocEntry, relLink } from 'templates/toc'
 import { TemplateContext } from 'templates/template'
-
+/**
+ * Output a `span` or plain text for the page title. If the page has a description,
+ * it's shown in the tooltip.
+ */
 const tocTitle = (entry: TocEntry) =>
     entry.desc ? 
-        `<span data-toggle="tooltip" data-title="${entry.desc}">${entry.page}</span>` :
+        html`<span data-toggle="tooltip" data-title="${entry.desc}">${entry.page}</span>` :
         entry.page
-
+/**
+ * Output a TOC entry as a list item.
+ */
 const tocEntry = (entry: TocEntry, relFileName: string) => html`
     <li>
-        ${entry.subs ? '<div class="accordion">' : ''}
+        ${entry.subs ? html`<div class="accordion">` : ''}
         ${entry.file ? 
-            `<a href="${relLink(relFileName, entry.file)}"${
-                relFileName == entry.file ? 
-                    ' class="highlight"' : ''
+            html`<a href="${relLink(relFileName, entry.file)}"${
+                relFileName == entry.file ? ' class="highlight"' : ''
                 }>${tocTitle(entry)}</a>` :
             entry.page}
         ${entry.subs ? 
-            html`</button>
+            html`</div>
             ${tocSection(entry.subs, relFileName)}` : 
             ''}
     </li>`
-
+/**
+ * Output a TOC section.
+ */
 const tocSection = (pages: Toc, relFileName: string) => html`
     <ul>
         ${pages.map(page => tocEntry(page, relFileName))}
     </ul>`
-
+/**
+ * ## Styles
+ */
 const styles = css`
 .tocmenu {
     --highlight-bar-width: 3px;
@@ -86,7 +99,9 @@ const styles = css`
 .tocmenu .accordion.collapsed:after {
     content: "\25BE";
 }`
-
+/**
+ * ## TOC Menu Component
+ */
 export default (ctx: TemplateContext) => {
     ctx.require('./tocmenu')
     ctx.style(styles)
