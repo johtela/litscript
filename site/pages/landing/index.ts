db536e8ca0cdba84ea80960275c4691fad8a1f2d
johtela <tjohtela@gmail.com>
2023-11-26 22:16:30 +0200

    Fixing mobile issues

--- site/pages/landing/index.ts
+++ site/pages/landing/index.ts
@@ -26,7 +26,7 @@ export default (ctx: TemplateContext) => {
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>
             ${ctx.frontMatter.projectName}${pageTitle(ctx.toc, ctx.relFilePath)}
